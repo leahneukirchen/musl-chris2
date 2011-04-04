2f7c3428f0308463e88f426caa867291c828e3fd
Christian Neukirchen <chneukirchen@gmail.com>
2011-04-04 14:14:54 +0200

    Revert "Add setresuid, setresgid"
    This reverts commit 631d3349efd841927ec47c33e0a991dde7efd87f.

--- src/unistd/setresuid.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include "syscall.h"
-#include "libc.h"
-
-int setresuid(uid_t ruid, uid_t euid, uid_t suid)
-{
-	if (libc.rsyscall) return libc.rsyscall(__NR_setresuid, ruid, euid, suid, 0, 0, 0);
-	return syscall(SYS_setresuid, ruid, euid, suid);
-}
